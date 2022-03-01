48b1629d71521a887f6f7ecd6d5c6060c77a3795
piyumi palihakkara <piyumipalihakkara765@gmail.com>
2022-02-28 17:29:33 -0800

    adding information

--- app/src/main/java/com/example/first/MainContactActivity.kt
+++ app/src/main/java/com/example/first/MainContactActivity.kt
@@ -7,5 +7,8 @@ class MainContactActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_contact)
+
+
+
     }
 }
